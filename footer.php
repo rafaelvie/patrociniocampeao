1cbe961097190ced966de981cfecc77230cf7313
Rafael Vieira <36818773+rafaelvie@users.noreply.github.com>
2020-05-14 12:09:22 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * The template for displaying the footer.
- *
- * Contains footer content and the closing of the
- * #main and #page div elements.
- *
- * @package WordPress
- * @subpackage classify
- * @since classify 1.0
- */
 ?>
 <?php 
 global $redux_demo; 
