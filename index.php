5d7940c4d397fdbcfacc3a6bc6765978b503db35
Rafael Vieira <36818773+rafaelvie@users.noreply.github.com>
2020-05-14 11:59:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ global $wp_query;
 		<div class="row">
 			<div class="col-md-8">
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>				
-					<?php get_template_part( 'templates/classify-blog-loop' ); ?>
+					<?php get_template_part( 'templates/blog-loop' ); ?>
 				<?php endwhile; ?>
 				<?php else : ?>
-				<p><?php esc_html_e( 'Sorry nothing found', 'classify' ); ?></p>
+				<p><?php esc_html_e( 'Sorry nothing found' ); ?></p>
 				<?php endif; ?>
 			</div><!--col-md-8-->
 			<div class="col-md-4">
